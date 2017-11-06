ffd33b34b4aaa520357f303a0669888416d41437
MilenaMR <30763392+MilenaMR@users.noreply.github.com>
2017-11-06 15:44:01 -0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -9,59 +9,29 @@
 #define TRUE 1
 #define FALSE 0
 
+
 int primo (int num){		
-/*   int divisor;		        
+  int divisor;		        
   int numdiv=0;     
   int Primo;			
 /* Primo = TRUE --> primo */
 /* Primo = FALSE --> nao é primo */
    
-  /*   
-    for(divisor=1; divisor < num+1; divisor++){
-              if(num % divisor == 0){
-                   numdiv++;
-              }                    
-    }
-    
-	      	      
-    if (numdiv==2){
-        Primo = 1; 
-//	printf("é primo\n");
-    }
-
+    if (num <= 1 || (num != 2 && num % 2 == 0)){
+	Primo = FALSE;}	/* nenhum numero inteiro <= 1 ou par > 2 e' primo */
     else{
-	Primo = 0; 
-//	printf("não é primo\n");
-    }
-*/
-	
-	int num,			/* numero a ser lido e testado */
-     d,				/* candidato a divisor */
-     EhPrimo;			/* para indicar se o numero dado n 'e primo ou nao     */
-	/* Convencao: EhPrimo = TRUE se o numero dado 'e primo */
-	/*  EhPrimo = FALSE se o numero dado nao 'e primo        */
-   
-
-   // printf("Forneca um numero inteiro positivo ===> ");
-    scanf("%d", &num);
-   // printf("Inteiro dado = %d \n", num);
-
-
-    if (num <= 1 || (num != 2 && num % 2 == 0))
-	EhPrimo = FALSE;	/* nenhum numero inteiro <= 1 ou par > 2 e' primo */
-    else
-	EhPrimo = TRUE;		/* o numero e' primo ate que se prove o contrario */
-
-
-    d = 3;
-    while (EhPrimo  && d <= num / 2) {
-	if (num % d == 0)
-	    EhPrimo = 0;
-	d = d + 2;		/* testamos so' os  impares: 3, 5, 7... */
+	Primo = TRUE;		/* o numero e' primo ate que se prove o contrario */
     }
 
-   
- return EhPrimo;
+    divisor = 3;
+    while (Primo  && divisor <= num/2) {
+	if (num % divisor == 0)
+	    Primo=0;
+	divisor = divisor + 2;		/* testamos so' os  impares: 3, 5, 7... */
+   }
+  
+
+ return Primo;
 } 
 
 int main() {
@@ -74,15 +44,14 @@ int main() {
    while (1){
  
 	scanf ("%d", &num);	  	
-// só aceita numeros positivo
+// quando recebe -1 para a ação
 	if (num == -1){	
-	     // printf("acabou\n");  		
 	      break;
   	}
 	else { 			
   
              if (primo (num) == 0){  			
-		fprintf (pipe,"%d\n",num);	  			  				  				  			//printf("estou aqui\n");
+		fprintf (pipe,"%d\n",num);	  			  				  				  
  	  				
               }
 	}
