172eae935f0aabe6e84a3e7620323d656c40c3a9
MilenaMR <30763392+MilenaMR@users.noreply.github.com>
2017-11-06 14:43:52 -0200

    teste

--- src/main.c
+++ src/main.c
@@ -9,15 +9,14 @@
 #define TRUE 1
 #define FALSE 0
 
-
 int primo (int num){		
-   int divisor;		        
+/*   int divisor;		        
   int numdiv=0;     
   int Primo;			
 /* Primo = TRUE --> primo */
 /* Primo = FALSE --> nao é primo */
    
-     
+  /*   
     for(divisor=1; divisor < num+1; divisor++){
               if(num % divisor == 0){
                    numdiv++;
@@ -34,8 +33,38 @@ int primo (int num){
 	Primo = 0; 
 //	printf("não é primo\n");
     }
+*/
+	
+	int num,			/* numero a ser lido e testado */
+     d,				/* candidato a divisor */
+     EhPrimo;			/* para indicar se o numero dado n 'e primo ou nao     */
+	/* Convencao: EhPrimo = TRUE se o numero dado 'e primo */
+	/*  EhPrimo = FALSE se o numero dado nao 'e primo        */
+   
 
- return Primo;
+    printf("Forneca um numero inteiro positivo ===> ");
+    scanf("%d", &n);
+    printf("Inteiro dado = %d \n", n);
+
+
+    if (num <= 1 || (num != 2 && num % 2 == 0))
+	EhPrimo = FALSE;	/* nenhum numero inteiro <= 1 ou par > 2 e' primo */
+    else
+	EhPrimo = TRUE;		/* o numero e' primo ate que se prove o contrario */
+
+
+    d = 3;
+    while (EhPrimo  && d <= num / 2) {
+	if (num % d == 0)
+	    EhPrimo = 0;
+	d = d + 2;		/* testamos so' os  impares: 3, 5, 7... */
+    }
+
+    if (EhPrimo)
+	printf("%d e' primo \n", num);
+    else
+	printf(" %d nao e' primo \n", num);
+ return EhPrimo;
 } 
 
 int main() {
